31aa2c12a89ba36ba761ad3e843611242da51891
Orlando <og17836@gmail.com>
2023-08-03 22:45:18 -0600

    Primer commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    
+  <h1 class="text-5xl text-center pt-24">Bienvenido seas a mi aplicación</h1>
+  <h1 class="text-8xl text-center pt-28">(°^-^°)</h1>
+
+
+@endsection
